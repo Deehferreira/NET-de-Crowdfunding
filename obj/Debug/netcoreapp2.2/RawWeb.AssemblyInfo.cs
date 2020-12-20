68f67f3e0547a1429910f170905a75446d32371b
Deehferreira <deborayferreirah@gmail.com>
2020-12-19 22:58:00 -0300

    meu primeiro commit

---
+++ obj/Debug/netcoreapp2.2/RawWeb.AssemblyInfo.cs
@@ -0,0 +1,13 @@
+
+
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("RawWeb")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("RawWeb")]
+[assembly: System.Reflection.AssemblyTitleAttribute("RawWeb")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+
